3c0926c29799ecb47b642b50fe75ffa2fc4439e2
Evan <contact@evandev.fr>
2023-04-12 11:32:39 +0200

    update sql

--- bdd.sql
+++ bdd.sql
@@ -1,55 +1,20 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le : mar. 11 avr. 2023 à 13:53
--- Version du serveur : 8.0.31
--- Version de PHP : 8.0.26
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Base de données : `authentification`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `articles`
---
-
-DROP TABLE IF EXISTS `articles`;
 CREATE TABLE IF NOT EXISTS `articles` (
   `id` int NOT NULL AUTO_INCREMENT,
   `contenue` text NOT NULL,
   `date_cree` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `img` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb3;
+) ENGINE=MyISAM AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb3;
 
---
--- Déchargement des données de la table `articles`
---
-
-INSERT INTO `articles` (`id`, `contenue`, `date_cree`, `img`) VALUES
-(10, '<p>dqzndqz</p><p>d</p><p>qzd</p><h2>qzqzdqzdqz</h2><p>dqz</p>', '2023-04-11 13:32:28', '6435616e8e5d0.jpg');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `users`
---
-
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `adresse_mail` varchar(200) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
@@ -58,18 +23,13 @@ CREATE TABLE IF NOT EXISTS `users` (
   `mdp` varchar(100) NOT NULL,
   `creation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `derniere_connexion` date NOT NULL,
+  `permission` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `adresse_mail` (`adresse_mail`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
 
---
--- Déchargement des données de la table `users`
---
-
-INSERT INTO `users` (`id`, `adresse_mail`, `nom`, `prenom`, `mdp`, `creation`, `derniere_connexion`) VALUES
-(1, 'contact@admin.com', 'admin', 'admin', '057ba03d6c44104863dc7361fe4578965d1887360f90a0895882e58a6248fc86', '2023-04-11 12:25:13', '2023-04-11');
-COMMIT;
-
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
