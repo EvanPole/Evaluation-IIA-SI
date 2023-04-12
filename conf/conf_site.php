630afb571b08dabee8f0fd47276ea5304e1614bd
Evan <contact@evandev.fr>
2023-04-12 14:24:16 +0200

    fix info leak

--- conf/conf_site.php
+++ conf/conf_site.php
@@ -22,5 +22,5 @@ try {
     $bdd = new PDO("mysql:host=$ip;dbname=$db;charset=utf8",$dbuser,$dbpass);
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo 'Connection failed';
+    die("Merci de contacter l'administrateur du site web");
 }
