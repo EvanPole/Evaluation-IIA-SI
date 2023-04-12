2872757b7a7da2cc4dc8ce6791fce44aeb3a9312
Evan <contact@evandev.fr>
2023-04-12 11:17:07 +0200

    add tentative / fix

--- login.php
+++ login.php
@@ -2,32 +2,44 @@
 session_start();
 require("./conf/conf_site.php");
 $erreur = '';
-if (isset($_POST["mail"]) && isset($_POST["mdp"])) {
-    $mail = $_POST["mail"];
-    $mdp = $_POST["mdp"];
-    $mdp = hash('sha256', $mdp);
-    $info_login = $bdd->prepare("SELECT * FROM users WHERE adresse_mail = ?");
-    $info_login->execute(array($mail));
-    if ($info_login->rowCount() > 0) {
-        $user_info = $info_login->fetch();
-        if ($mdp == $user_info["mdp"]) {
-            $_SESSION["id"] = $user_info["id"];
-            $_SESSION["adresse_mail"] = $user_info["adresse_mail"];
-            $_SESSION["nom"] = $user_info["nom"];
-            $_SESSION["prenom"] = $user_info["prenom"];
-            $_SESSION["permission"] = $user_info["permission"];
-            $info_co = $bdd->prepare("UPDATE users SET derniere_connexion = ? WHERE adresse_mail = ?");
-            $date = date('Y-m-d H:i:s');
-            $info_co->execute(array($date, $mail));
-            
-            header("Location: ./admin/overview.php");
-        }else{
-            $erreur = "Le mdp ne correspond pas !";
+if(empty($_SESSION['tentative'])){
+    $_SESSION['tentative'] = 0;
+}
+$_SESSION['tentative'] += 1;
+
+if ($_SESSION['tentative'] <= 15) {
+        if (isset($_POST["mail"]) && isset($_POST["mdp"])) {
+            $mail = $_POST["mail"];
+            $mdp = $_POST["mdp"];
+            $mdp = hash('sha256', $mdp);
+            $info_login = $bdd->prepare("SELECT * FROM users WHERE adresse_mail = ?");
+            $info_login->execute(array($mail));
+            if ($info_login->rowCount() > 0) {
+                $user_info = $info_login->fetch();
+                if ($mdp == $user_info["mdp"]) {
+                    $_SESSION["id"] = $user_info["id"];
+                    $_SESSION["adresse_mail"] = $user_info["adresse_mail"];
+                    $_SESSION["nom"] = $user_info["nom"];
+                    $_SESSION["prenom"] = $user_info["prenom"];
+                    $_SESSION["permission"] = $user_info["permission"];
+                    $info_co = $bdd->prepare("UPDATE users SET derniere_connexion = ? WHERE adresse_mail = ?");
+                    $date = date('Y-m-d H:i:s');
+                    $info_co->execute(array($date, $mail));
+
+                    header("Location: ./admin/overview.php");
+                } else {
+                    $erreur = "Le mdp ne correspond pas !";
+                }
+            } else {
+                $erreur = "L'utilisateur n'existe pas";
+            }
+        } else {
+            $erreur = "Merci de remplire tous les champs";
         }
     } else {
-        $erreur = "L'utilisateur n'existe pas";
+        $erreur = "Trop de tentatives";
     }
-}
+
 
 ?>
 
@@ -44,15 +56,15 @@ if (isset($_POST["mail"]) && isset($_POST["mdp"])) {
 
 <body>
     <div class="main-container">
-    <form method="post">
-        <h2>Login</h2>
-        <input type="text" placeholder="mail" name="mail">
-        <input type="password"  placeholder="Mot de passe" name="mdp">
-        <button type="submit">Continuer</button>
-        <p style="color: red;"><?= $erreur ?></p>
-    </form>
-    <a href="public/blog.php">Retour au blog</a>
-    <a href="register.php">Inscription</a>
+        <form method="post">
+            <h2>Login</h2>
+            <input type="text" placeholder="mail" name="mail">
+            <input type="password" placeholder="Mot de passe" name="mdp">
+            <button type="submit">Continuer</button>
+            <p style="color: red;"><?= $erreur ?></p>
+        </form>
+        <a href="public/blog.php">Retour au blog</a>
+        <a href="register.php">Inscription</a>
     </div>
 </body>
 
